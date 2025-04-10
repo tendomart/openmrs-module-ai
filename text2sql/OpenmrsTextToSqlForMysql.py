c7519e409edb546f9bfc89f664107d2f1f528231
tendomart <tmkmart@gmail.com>
2025-04-10 14:28:08 +0300

    use deepseek-coder:6.7b-base

--- text2sql/OpenmrsTextToSqlForMysql.py
+++ text2sql/OpenmrsTextToSqlForMysql.py
@@ -18,7 +18,7 @@ from DatabaseTables import openmrs_tables
 #     provider="ollama"
 # )
 
-model=LiteLLMModel(model_id="ollama/qwen2.5-coder:latest"
+model=LiteLLMModel(model_id="ollama/deepseek-coder:6.7b-base"
                    # api_key="ollama"
                    )
 
