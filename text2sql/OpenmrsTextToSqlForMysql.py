e848a6bb8faae5a3d7f9ea550c1d090aa05c4980
tendomart <tmkmart@gmail.com>
2025-04-10 15:11:28 +0300

    qwen2.5-coder:1.5b

--- text2sql/OpenmrsTextToSqlForMysql.py
+++ text2sql/OpenmrsTextToSqlForMysql.py
@@ -18,7 +18,7 @@ from DatabaseTables import openmrs_tables
 #     provider="ollama"
 # )
 
-model=LiteLLMModel(model_id="ollama/llama3.2:3b",
+model=LiteLLMModel(model_id="qwen2.5-coder:1.5b",
                    api_key="ollama"
                    )
 
